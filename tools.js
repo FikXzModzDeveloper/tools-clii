d572e98b36dcd4d24a19194f973fc5491206b23d
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-10-01 15:17:00 +0700

    kontl

--- tools.js
+++ tools.js
@@ -1,8 +1,9 @@
+const { openBo } = require('./lib/openChannel');
 const { LihatJawa } = require('./lib/banner');
 const TampilMenuGengsi = require('./modules/menu');
-const openBo = require('./lib/openChannel');
 
 // meluncurkan muwani
 console.clear();
+openBo();
 LihatJawa();
 TampilMenuGengsi().catch(console.error);
