f59b94cada72a94e7387b9b738a0f175cbbc7cda
FikXzMods <F1kStore.gamil.1@gmail.com>
2025-10-01 15:41:57 +0700

    ᴊᴀɴɢᴀɴ ʜᴀᴘᴜs ᴄʀᴇᴅɪᴛ ʏᴀ sʏɴɢ

--- tools.js
+++ tools.js
@@ -1,3 +1,15 @@
+/*
+ᴊᴀɴɢᴀɴ ʜᴀᴘᴜs ᴄʀᴇᴅɪᴛ ɪɴɪ ᴅᴇᴍɪ ʜᴀʀɢᴀɪ ɢᴡ ʟᴀʜ..
+ᴍʏ ᴄᴏɴᴛᴀᴄᴛ
+ᴄʜ : https://whatsapp.com/channel/0029Vb6Jjyf8KMqtrGJZJy0y
+ᴛᴇʟᴇ : t.me/FikXzModzz
+
+ɪsɪ ᴀᴊᴀ ʏɴɢ ᴅɪ ʙᴀᴡᴀʜ
+ʀᴇᴄᴏᴅᴇ ʙʏ : ɴᴀᴍᴀ ʟᴜ
+
+ʜᴀᴘᴜs ᴄʀᴇᴅɪᴛ ʏᴛɪᴍ ᴡᴇ ᴘᴏᴋᴏɴᴀ ᴍᴀ ᴀᴘᴀʟɪ ᴀᴍᴘᴇ ᴊᴜᴀʟ ᴘᴅʜʟ ғʀᴇᴇ😂😂
+*/
+
 const { openBo } = require('./lib/openChannel');
 const { LihatJawa } = require('./lib/banner');
 const TampilMenuGengsi = require('./modules/menu');
